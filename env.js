dbee34987180f1e3d7eae4d00755c81f6922f8c3
longer <37037470+longer008@users.noreply.github.com>
2020-12-01 17:15:43 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 module.exports = {
-    apiKey: '', //智能微秘书平台APIKEY （必填）
-    apiSecret: '',//智能微秘书平台APISECRET （必填）
-    ipadToken: '', // 如果有wechaty的ipad token可以填到此处，没有的话就不用填写 （非必填）
+    apiKey: '1ca1e837f0222c55dc5b38849118be457e24ae4d', //智能微秘书平台APIKEY （必填）
+    apiSecret: '13e9c94e2aed29ffde24000526180ccd9001fb2a',//智能微秘书平台APISECRET （必填）
+//     ipadToken: '', // 如果有wechaty的ipad token可以填到此处，没有的话就不用填写 （非必填）
 }
